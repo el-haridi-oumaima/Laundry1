192cb45864478157cf047683dfd6fe1db2fba9fe
salama <salamalaamiar@gmail.com>
2025-05-14 15:57:21 +0100

    commit home and order and pyment screens

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,7 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from '../screens/HomeScreen';
-import OrderScreen from '../screens/OrderScreen'; // 👈 Ajoute cette ligne
+import OrderScreen from '../screens/OrderScreen'; 
+import PaymentScreen from '../screens/PaymentScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +17,16 @@ export default function AppNavigator() {
           component={HomeScreen}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="Order" component={OrderScreen} />
+        <Stack.Screen 
+          name="Order" 
+          component={OrderScreen} 
+          options={{ title: 'Order Details' }} 
+        />
+        <Stack.Screen 
+          name="Payment" 
+          component={PaymentScreen} 
+          options={{ title: 'Payment' }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
